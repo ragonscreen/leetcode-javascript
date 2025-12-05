9287801eba0fc407f97e9609c50c8e11e4f343aa
Anurag Das <ragonscreen@gmail.com>
2025-12-05 15:20:12 +0530

    feat: solve 242 - Valid Anagram - Easy

---
+++ tests/0242_valid-anagram.test.js
@@ -0,0 +1,12 @@
+import { describe, expect, test } from 'vitest';
+import isAnagram from '../src/problems/solved/0242_valid-anagram.js';
+
+describe('isAnagram', () => {
+        test('returns true for anagrams', () => {
+                expect(isAnagram('anagram', 'nagaram')).toBe(true);
+        });
+
+        test('returns false for non anagrams', () => {
+                expect(isAnagram('rat', 'car')).toBe(false);
+        });
+});
