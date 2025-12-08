93c5ad4a41c528b1c24b71902bc4681c7ae5e998
Anurag Das <ragonscreen@gmail.com>
2025-12-08 16:14:39 +0530

    feat: solve 49 - Group Anagrams - Medium; feat: add test util function `arrDeepSortAlpha`

---
+++ src/problems/solved/0049_group-anagrams.js
@@ -0,0 +1,41 @@
+/**
+ * 0049. Group Anagrams
+ * Link: https://leetcode.com/problems/group-anagrams/
+ * Difficulty: Medium
+ * Date: 2025-12-08
+ * Author: ragonscreen
+ */
+
+/**
+ * Implementation: Hashing with Prime Numbers
+ * Time Complexity: O(n * m)
+ * Space Complexity: O(n)
+ * n === strs.length; m === length of largest string in strs
+ *
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+const groupAnagrams = (strs) => {
+        const groups = {};
+        const primes = [
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+                61, 67, 71, 73, 79, 83, 89, 97, 101,
+        ];
+
+        for (const str of strs) {
+                let hash = 1;
+
+                for (const c of str) {
+                        const code = c.charCodeAt() - 'a'.charCodeAt();
+                        hash *= primes[code];
+                        hash %= 1e9 + 7;
+                }
+
+                groups[hash] ??= [];
+                groups[hash].push(str);
+        }
+
+        return Object.values(groups);
+};
+
+export default groupAnagrams;
--- tests/utils/arrays.js
+++ tests/utils/arrays.js
@@ -2,4 +2,22 @@ const arrSortNum = (arr, descending = false) => {
         return [...arr].sort((a, b) => (descending ? b - a : a - b));
 };
 
-export { arrSortNum };
+const arrDeepSortAlpha = (arr) => {
+        const arrCpy = [...arr];
+
+        for (let i = 0; i < arrCpy.length; i++) {
+                if (Array.isArray(arrCpy[i])) {
+                        arrCpy[i] = arrDeepSortAlpha(arrCpy[i]);
+                }
+        }
+
+        return arrCpy.sort((a, b) => {
+                if (Array.isArray(a)) {
+                        return a.length - b.length;
+                }
+
+                return a.localeCompare(b);
+        });
+};
+
+export { arrSortNum, arrDeepSortAlpha };
