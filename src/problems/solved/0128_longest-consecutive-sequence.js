cb1e94558ec0833c4f2323a36e9466d350f08589
Anurag Das <ragonscreen@gmail.com>
2025-12-16 15:01:25 +0530

    feat: solve 0128 - Longest Consecutive Sequence - Medium

---
+++ src/problems/solved/0128_longest-consecutive-sequence.js
@@ -0,0 +1,40 @@
+/**
+ * 0128. Longest Consecutive Sequence
+ * Link: https://leetcode.com/problems/longest-consecutive-sequence/
+ * Difficulty: Medium
+ * Date: 2025-12-16
+ * Author: ragonscreen
+ */
+
+/**
+ * Implementation: Hash Set
+ * Time Complexity: O()
+ * Space Complexity: O()
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestConsecutive = (nums) => {
+        const set = new Set(nums);
+        let max = 0;
+
+        for (const n of set) {
+                if (set.has(n - 1)) {
+                        continue;
+                }
+
+                let len = 1;
+
+                while (set.has(n + len)) {
+                        len++;
+                }
+
+                if (len > max) {
+                        max = len;
+                }
+        }
+
+        return max;
+};
+
+export default longestConsecutive;
---
+++ tests/0128_longest-consecutive-sequence.test.js
@@ -0,0 +1,24 @@
+import { describe, expect, test } from 'vitest';
+import longestConsecutive from '../src/problems/solved/0128_longest-consecutive-sequence.js';
+
+describe('longestConsecutive', () => {
+        test('returns the longest consecutive sequence', () => {
+                expect(
+                        longestConsecutive([100, 4, 200, 1, 3, 2]),
+                ).toStrictEqual(4);
+        });
+
+        test('handles long sequences', () => {
+                expect(
+                        longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]),
+                ).toStrictEqual(9);
+        });
+
+        test('handles inputs with duplicate elements', () => {
+                expect(longestConsecutive([1, 0, 1, 2])).toStrictEqual(3);
+        });
+
+        test('handles inputs with no elements', () => {
+                expect(longestConsecutive([])).toStrictEqual(0);
+        });
+});
