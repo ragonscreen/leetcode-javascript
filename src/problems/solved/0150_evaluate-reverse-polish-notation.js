67715ed61cdffc0ad1a72de1e48007548735fd27
Anurag Das <ragonscreen@gmail.com>
2026-01-05 22:44:03 +0530

    feat: solve 0150 - Evaluate Reverse Polish Notation - Medium

---
+++ src/problems/solved/0150_evaluate-reverse-polish-notation.js
@@ -0,0 +1,54 @@
+/**
+ * 0150. Evaluate Reverse Polish Notation
+ * Link: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+ * Difficulty: Medium
+ * Date: 2026-01-05
+ * Author: ragonscreen
+ */
+
+/**
+ * Implementation: Stack
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
+ * @param {string[]} tokens
+ * @return {number}
+ */
+const evalRPN = (tokens) => {
+        const stack = [];
+        const operators = ['+', '-', '*', '/'];
+
+        for (const token of tokens) {
+                if (!operators.includes(token)) {
+                        stack.push(token);
+                        continue;
+                }
+
+                const b = Number(stack.pop());
+                const a = Number(stack.pop());
+                let res;
+
+                switch (token) {
+                        case '+':
+                                res = a + b;
+                                break;
+                        case '-':
+                                res = a - b;
+                                break;
+                        case '*':
+                                res = a * b;
+                                break;
+                        case '/':
+                                res = Math.trunc(a / b);
+                                break;
+                        default:
+                                break;
+                }
+
+                stack.push(res);
+        }
+
+        return Number(stack[0]);
+};
+
+export default evalRPN;
