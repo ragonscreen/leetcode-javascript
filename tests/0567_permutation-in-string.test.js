ac01c11569655710c2f098d23cca3abc70101a1e
Anurag Das <ragonscreen@gmail.com>
2026-01-27 22:01:48 +0530

    feat: solve 0567 - Permutation in String - Medium

---
+++ tests/0567_permutation-in-string.test.js
@@ -0,0 +1,89 @@
+// biome-ignore-all lint/complexity/noExcessiveLinesPerFunction: multiple tests in one describe function
+
+import { describe, expect, test } from 'vitest';
+import {
+        checkInclusion,
+        checkInclusion1,
+} from '../src/problems/solved/0567_permutation-in-string.js';
+
+describe('checkInclusion', () => {
+        describe('checkInclusion', () => {
+                test('basic test 1', () => {
+                        expect(checkInclusion('ab', 'eidbaooo')).toStrictEqual(
+                                true,
+                        );
+                });
+
+                test('basic test 2', () => {
+                        expect(checkInclusion('ab', 'eidboaoo')).toStrictEqual(
+                                false,
+                        );
+                });
+
+                test('basic test 3', () => {
+                        expect(checkInclusion('abc', 'bbbca')).toStrictEqual(
+                                true,
+                        );
+                });
+
+                test('basic test 4', () => {
+                        expect(
+                                checkInclusion(
+                                        'trinitrophenylmethylnitramine',
+                                        'dinitrophenylhydrazinetrinitrophenylmethylnitramine',
+                                ),
+                        ).toStrictEqual(true);
+                });
+
+                test('custom test 1', () => {
+                        expect(checkInclusion('abc', 'a')).toStrictEqual(false);
+                });
+
+                test('custom test 2', () => {
+                        expect(checkInclusion('abc', 'cab')).toStrictEqual(
+                                true,
+                        );
+                });
+        });
+
+        describe('checkInclusion1', () => {
+                test('basic test 1', () => {
+                        expect(checkInclusion1('ab', 'eidbaooo')).toStrictEqual(
+                                true,
+                        );
+                });
+
+                test('basic test 2', () => {
+                        expect(checkInclusion1('ab', 'eidboaoo')).toStrictEqual(
+                                false,
+                        );
+                });
+
+                test('basic test 3', () => {
+                        expect(checkInclusion1('abc', 'bbbca')).toStrictEqual(
+                                true,
+                        );
+                });
+
+                test('basic test 4', () => {
+                        expect(
+                                checkInclusion1(
+                                        'trinitrophenylmethylnitramine',
+                                        'dinitrophenylhydrazinetrinitrophenylmethylnitramine',
+                                ),
+                        ).toStrictEqual(true);
+                });
+
+                test('custom test 1', () => {
+                        expect(checkInclusion1('abc', 'a')).toStrictEqual(
+                                false,
+                        );
+                });
+
+                test('custom test 2', () => {
+                        expect(checkInclusion1('abc', 'cab')).toStrictEqual(
+                                true,
+                        );
+                });
+        });
+});
