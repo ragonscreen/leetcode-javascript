8edf048ad325341502438b9bce21f52d4053e326
Anurag Das <ragonscreen@gmail.com>
2026-01-30 00:28:10 +0530

    feat: solve 0451 - Sort Characters By Frequency - Medium

---
+++ tests/0451_sort-characters-by-frequency.test.js
@@ -0,0 +1,22 @@
+import { describe, expect, test } from 'vitest';
+import { frequencySort } from '../src/problems/solved/0451_sort-characters-by-frequency.js';
+
+describe('frequencySort', () => {
+        test('basic test 1', () => {
+                expect(frequencySort('tree')).toBeOneOf(['eert', 'eetr']);
+        });
+
+        test('basic test 2', () => {
+                expect(frequencySort('cccaaa')).toBeOneOf(['aaaccc', 'cccaaa']);
+        });
+
+        test('basic test 3', () => {
+                expect(frequencySort('Aabb')).toBeOneOf(['bbAa', 'bbaA']);
+        });
+
+        test('basic test 4', () => {
+                expect(
+                        frequencySort('2a554442f544asfasssffffasss'),
+                ).toStrictEqual('sssssssffffff44444aaaa55522');
+        });
+});
