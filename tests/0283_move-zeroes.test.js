243ab04fed979363cbc54ef7a61f720689ba8fee
Anurag Das <ragonscreen@gmail.com>
2026-02-02 21:06:28 +0530

    feat: solve 0283 - Move Zeroes - Easy

---
+++ tests/0283_move-zeroes.test.js
@@ -0,0 +1,14 @@
+import { describe, expect, test } from 'vitest';
+import { moveZeroes } from '../src/problems/solved/0283_move-zeroes.js';
+
+describe('moveZeroes', () => {
+        test('basic test 1', () => {
+                expect(moveZeroes([0, 1, 0, 3, 12])).toStrictEqual([
+                        1, 3, 12, 0, 0,
+                ]);
+        });
+
+        test('basic test 1', () => {
+                expect(moveZeroes([0])).toStrictEqual([0]);
+        });
+});
