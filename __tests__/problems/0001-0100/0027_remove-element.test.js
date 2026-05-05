75a41628ce3355668c8f860f7222a8d4744b52d7
Anurag Das <ragonscreen@gmail.com>
2026-05-05 19:19:21 +0530

    fix: revise 0027 - Remove Element - Easy

--- __tests__/problems/0001-0100/0027_remove-element.test.js
+++ __tests__/problems/0001-0100/0027_remove-element.test.js
@@ -2,12 +2,11 @@ import { describe, expect, test } from 'bun:test';
 import { removeElement } from '../../../src/problems/0001-0100/0027_remove-element.js';
 
 const testcases = [
-        { nums: [3, 2, 2, 3], val: 3, expected: 2, expectedNums: [2, 2] },
+        { nums: [3, 2, 2, 3], val: 3, expected: { len: 2, nums: [2, 2] } },
         {
                 nums: [0, 1, 2, 2, 3, 0, 4, 2],
                 val: 2,
-                expected: 5,
-                expectedNums: [0, 1, 3, 0, 4],
+                expected: { len: 5, nums: [0, 1, 3, 0, 4] },
         },
 ];
 
@@ -18,13 +17,12 @@ describe('removeElement', () => {
                 nums,
                 val,
                 expected,
-                expectedNums,
         }) => {
                 const k = removeElement(nums, val);
-                expect(k).toStrictEqual(expected);
+                expect(k).toStrictEqual(expected.len);
 
                 for (let i = 0; i < k; i++) {
-                        expect(nums[i]).toStrictEqual(expectedNums[i]);
+                        expect(nums[i]).toStrictEqual(expected.nums[i]);
                 }
         });
 });
