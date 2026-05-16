c7459752a62069cbd8060ba8c2b0f7963ff56be5
Anurag Das <ragonscreen@gmail.com>
2026-05-16 21:54:20 +0530

    fix: revise 0174 - Dungeon Game - Hard

--- __tests__/problems/0101-0200/0174_dungeon-game.test.js
+++ __tests__/problems/0101-0200/0174_dungeon-game.test.js
@@ -2,12 +2,24 @@ import { describe, expect, test } from 'bun:test';
 import { calculateMinimumHP } from '../../../src/problems/0101-0200/0174_dungeon-game.js';
 
 const testcases = [
-        { dungeon: [[-2,-3,3],[-5,-10,1],[10,30,-5]], expected: 7 },
+        {
+                dungeon: [
+                        [-2, -3, 3],
+                        [-5, -10, 1],
+                        [10, 30, -5],
+                ],
+                expected: 7,
+        },
         { dungeon: [[0]], expected: 1 },
 ];
 
 describe('calculateMinimumHP', () => {
-        test.each(structuredClone(testcases))('calculateMinimumHP($dungeon) -> $expected', ({ dungeon, expected }) => {
+        test.each(
+                structuredClone(testcases),
+        )('calculateMinimumHP($dungeon) -> $expected', ({
+                dungeon,
+                expected,
+        }) => {
                 expect(calculateMinimumHP(dungeon)).toStrictEqual(expected);
         });
 });
