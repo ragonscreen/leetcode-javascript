bb8a635ac2f9e39cd6a94f2b97a0c7ab40f300ee
Anurag Das <ragonscreen@gmail.com>
2026-05-19 12:44:56 +0530

    feat: solve 2540 - Minimum Common Value - Easy

---
+++ src/problems/2501-2600/2540_minimum-common-value.js
@@ -0,0 +1,58 @@
+/**
+ * 2540. Minimum Common Value
+ *
+ * Link: https://leetcode.com/problems/minimum-common-value/
+ * Category: Algorithms
+ * Difficulty: Easy
+ * Date: 2026-05-19
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Array (topic_5)
+ * - Hash Table (topic_6)
+ * - Two Pointers (topic_9)
+ * - Binary Search (topic_11)
+ *
+ * Stats:
+ * - Total Accepted: 353,479
+ * - Total Submissions: 596,827
+ * - Acceptance Rate: 59.2%
+ *
+ * Similar Problems:
+ * - intersection-of-two-arrays (Easy)
+ * - intersection-of-two-arrays-ii (Easy)
+ */
+
+/**
+ * Approach: Two Pointers
+ * Time Complexity: O(n + m)
+ * Space Complexity: O(1)
+ * `n` = length of `nums1`, `m` = length of `nums2`
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const getCommon = (nums1, nums2) => {
+        let p = 0;
+        let q = 0;
+
+        while (p < nums1.length && q < nums2.length) {
+                const a = nums1[p];
+                const b = nums2[q];
+
+                if (a === b) {
+                        return a;
+                }
+
+                if (a < b) {
+                        p++;
+                } else {
+                        q++;
+                }
+        }
+
+        return -1;
+};
+
+export { getCommon };
