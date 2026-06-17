7d0e3360405acdcec0e0df5a00b694d1b38ab6f0
Anurag Das <ragonscreen@gmail.com>
2026-06-18 02:03:25 +0530

    feat: solve 3614 - Process String with Special Operations II - Hard

---
+++ __tests__/problems/3601-3700/3614_process-string-with-special-operations-ii.test.js
@@ -0,0 +1,30 @@
+import { describe, expect, test } from 'bun:test';
+import { processStr } from '../../../src/problems/3601-3700/3614_process-string-with-special-operations-ii.js';
+
+const testcases = [
+        { s: 'a#b%*', k: 1, expected: 'a' },
+        { s: 'cd%#*#', k: 3, expected: 'd' },
+        { s: 'z*#', k: 0, expected: '.' },
+        { s: 'abc#d#e', k: 10, expected: 'a' },
+        { s: 'cde##*%', k: 10, expected: 'c' },
+        { s: 'cde##*', k: 10, expected: 'd' },
+        { s: 'qe*vkg', k: 0, expected: 'q' },
+        { s: '#jief%k', k: 0, expected: 'f' },
+        { s: '#jief%k*', k: 3, expected: 'j' },
+        { s: '#ji*ef%k*', k: 2, expected: 'j' },
+        { s: 'wuxi##%u#%', k: 18, expected: 'w' },
+        { s: 'h#a#%r##c#', k: 2, expected: 'h' },
+        {
+                s: '%edx#n#lkc####uom##qg#%#b#ek%##%%ocr#m%#fv%i%%#n#u%%#n#q%v#rwvd##t###%#%%%o*##r#gr*gz#dm%ez',
+                k: 4780,
+                expected: 'd',
+        },
+];
+
+describe('processStr', () => {
+        test.each(
+                structuredClone(testcases),
+        )('processStr($s, $k) -> $expected', ({ s, k, expected }) => {
+                expect(processStr(s, k)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/3601-3700/3614_process-string-with-special-operations-ii.js
@@ -0,0 +1,75 @@
+/**
+ * 3614. Process String with Special Operations II
+ *
+ * Link: https://leetcode.com/problems/process-string-with-special-operations-ii/
+ * Category: Algorithms
+ * Difficulty: Hard
+ * Date: 2026-06-17
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - String (topic_10)
+ * - Simulation (topic_61055)
+ * - Senior Staff (position_senior-staff)
+ * - Weekly Contest 458 (contest_weekly-contest-458)
+ *
+ * Stats:
+ * - Total Accepted: 13,295
+ * - Total Submissions: 71,775
+ * - Acceptance Rate: 18.5%
+ */
+
+/**
+ * Approach: Simulation
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * `n` = `s.length`
+ *
+ * @param {string} s
+ * @param {number} k
+ * @return {character}
+ */
+const processStr = (s, k) => {
+        const n = s.length;
+        let len = 0;
+
+        for (let i = 0; i < n; i++) {
+                const c = s[i];
+
+                if (c === '*') {
+                        len = Math.max(0, len - 1);
+                } else if (c === '#') {
+                        len *= 2;
+                } else if (c !== '%') {
+                        len++;
+                }
+        }
+
+        if (k >= len) {
+                return '.';
+        }
+
+        for (let i = n - 1; i > -1; i--) {
+                const c = s[i];
+
+                if (c === '*') {
+                        len++;
+                } else if (c === '#') {
+                        len /= 2;
+
+                        if (k >= len) {
+                                k -= len;
+                        }
+                } else if (c === '%') {
+                        k = len - k - 1;
+                } else {
+                        len--;
+
+                        if (k === len) {
+                                return c;
+                        }
+                }
+        }
+};
+
+export { processStr };
