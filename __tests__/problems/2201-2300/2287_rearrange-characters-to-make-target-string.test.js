907b1b4a6ddc71eec044ee279ef20ab4ce779388
Anurag Das <ragonscreen@gmail.com>
2026-07-23 22:41:25 +0530

    feat: solve 2287 - Rearrange Characters to Make Target String - Easy

---
+++ __tests__/problems/2201-2300/2287_rearrange-characters-to-make-target-string.test.js
@@ -0,0 +1,21 @@
+import { describe, expect, test } from 'bun:test';
+import { rearrangeCharacters } from '../../../src/problems/2201-2300/2287_rearrange-characters-to-make-target-string.js';
+
+const testcases = [
+        { s: 'ilovecodingonleetcode', target: 'code', expected: 2 },
+        { s: 'abcba', target: 'abc', expected: 1 },
+        { s: 'abbaccaddaeea', target: 'aaaaa', expected: 1 },
+        { s: 'aaaaa', target: 'a', expected: 5 },
+];
+
+describe('rearrangeCharacters', () => {
+        test.each(
+                structuredClone(testcases),
+        )('rearrangeCharacters($s, $target) -> $expected', ({
+                s,
+                target,
+                expected,
+        }) => {
+                expect(rearrangeCharacters(s, target)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/2201-2300/2287_rearrange-characters-to-make-target-string.js
@@ -0,0 +1,64 @@
+/**
+ * 2287. Rearrange Characters to Make Target String
+ *
+ * Link: https://leetcode.com/problems/rearrange-characters-to-make-target-string/
+ * Category: Algorithms
+ * Difficulty: Easy
+ * Date: 2026-07-23
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Hash Table (topic_6)
+ * - String (topic_10)
+ * - Counting (topic_61062)
+ * - Mid Level (position_mid-level)
+ * - Weekly Contest 295 (contest_weekly-contest-295)
+ *
+ * Stats:
+ * - Total Accepted: 64,480
+ * - Total Submissions: 102,246
+ * - Acceptance Rate: 63.1%
+ *
+ * Similar Problems:
+ * - find-words-that-can-be-formed-by-characters (Easy)
+ * - maximum-number-of-occurrences-of-a-substring (Medium)
+ */
+
+/**
+ * Approach: Simulation
+ * Time Complexity: O(n + m)
+ * Space Complexity: O(1)
+ * `n` = `s.length`, `m` = `target.length`
+ *
+ * @param {string} s
+ * @param {string} target
+ * @return {number}
+ */
+const rearrangeCharacters = (s, target) => {
+        const n = s.length;
+        const m = target.length;
+        const cnts = new Uint32Array(26);
+        const cntt = new Uint32Array(26);
+
+        for (let i = 0; i < n; i++) {
+                cnts[s.charCodeAt(i) - 97]++;
+        }
+
+        for (let i = 0; i < m; i++) {
+                cntt[target.charCodeAt(i) - 97]++;
+        }
+
+        let res = n;
+
+        for (let i = 0; i < 26; i++) {
+                if (cntt[i] === 0) {
+                        continue;
+                }
+
+                res = Math.min(res, Math.floor(cnts[i] / cntt[i]));
+        }
+
+        return res;
+};
+
+export { rearrangeCharacters };
