e97040de04107d4f378b2d251e73986e4e40f7bd
Anurag Das <ragonscreen@gmail.com>
2026-07-23 22:33:40 +0530

    feat: solve 1189 - Maximum Number of Balloons - Easy

---
+++ __tests__/problems/1101-1200/1189_maximum-number-of-balloons.test.js
@@ -0,0 +1,16 @@
+import { describe, expect, test } from 'bun:test';
+import { maxNumberOfBalloons } from '../../../src/problems/1101-1200/1189_maximum-number-of-balloons.js';
+
+const testcases = [
+        { text: 'nlaebolko', expected: 1 },
+        { text: 'loonbalxballpoon', expected: 2 },
+        { text: 'leetcode', expected: 0 },
+];
+
+describe('maxNumberOfBalloons', () => {
+        test.each(
+                structuredClone(testcases),
+        )('maxNumberOfBalloons($text) -> $expected', ({ text, expected }) => {
+                expect(maxNumberOfBalloons(text)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/1101-1200/1189_maximum-number-of-balloons.js
@@ -0,0 +1,42 @@
+/**
+ * 1189. Maximum Number of Balloons
+ *
+ * Link: https://leetcode.com/problems/maximum-number-of-balloons/
+ * Category: Algorithms
+ * Difficulty: Easy
+ * Date: 2026-06-23
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Hash Table (topic_6)
+ * - String (topic_10)
+ * - Counting (topic_61062)
+ * - Mid Level (position_mid-level)
+ * - Weekly Contest 154 (contest_weekly-contest-154)
+ *
+ * Stats:
+ * - Total Accepted: 498,331
+ * - Total Submissions: 776,131
+ * - Acceptance Rate: 64.2%
+ */
+
+/**
+ * Approach: Simulation
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * `n` = `text.length`
+ *
+ * @param {string} text
+ * @return {number}
+ */
+const maxNumberOfBalloons = (text) => {
+        const cnt = new Uint32Array(26);
+
+        for (let i = 0; i < text.length; i++) {
+                cnt[text.charCodeAt(i) - 97]++;
+        }
+
+        return Math.min(cnt[1], cnt[0], cnt[11] >> 1, cnt[14] >> 1, cnt[13]);
+};
+
+export { maxNumberOfBalloons };
