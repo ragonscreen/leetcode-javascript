bee4fdbc456be5b3990ee1a5dd11805d57858f83
Anurag Das <ragonscreen@gmail.com>
2026-07-26 09:21:43 +0530

    feat: solve 1846 - Maximum Element After Decreasing and Rearranging - Medium

---
+++ __tests__/problems/1801-1900/1846_maximum-element-after-decreasing-and-rearranging.test.js
@@ -0,0 +1,22 @@
+import { describe, expect, test } from 'bun:test';
+import { maximumElementAfterDecrementingAndRearranging } from '../../../src/problems/1801-1900/1846_maximum-element-after-decreasing-and-rearranging.js';
+
+const testcases = [
+        { arr: [2, 2, 1, 2, 1], expected: 2 },
+        { arr: [100, 1, 1000], expected: 3 },
+        { arr: [1, 2, 3, 4, 5], expected: 5 },
+        { arr: [73, 98, 9], expected: 3 },
+];
+
+describe('maximumElementAfterDecrementingAndRearranging', () => {
+        test.each(
+                structuredClone(testcases),
+        )('maximumElementAfterDecrementingAndRearranging($arr) -> $expected', ({
+                arr,
+                expected,
+        }) => {
+                expect(
+                        maximumElementAfterDecrementingAndRearranging(arr),
+                ).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/1801-1900/1846_maximum-element-after-decreasing-and-rearranging.js
@@ -0,0 +1,53 @@
+/**
+ * 1846. Maximum Element After Decreasing and Rearranging
+ *
+ * Link: https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+ * Category: Algorithms
+ * Difficulty: Medium
+ * Date: 2026-07-26
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Array (topic_5)
+ * - Greedy (topic_17)
+ * - Sorting (topic_61049)
+ * - Staff (position_staff)
+ * - Biweekly Contest 51 (contest_biweekly-contest-51)
+ *
+ * Stats:
+ * - Total Accepted: 225,085
+ * - Total Submissions: 325,471
+ * - Acceptance Rate: 69.2%
+ */
+
+/**
+ * Approach: Sorting
+ * Time Complexity: O(n lg n)
+ * Space Complexity: O(1) auxiliary, O(n) for sorting
+ * `n` = `arr.length`
+ *
+ * @param {number[]} arr
+ * @return {number}
+ */
+const maximumElementAfterDecrementingAndRearranging = (arr) => {
+        arr.sort((a, b) => a - b);
+        let cur = 0;
+
+        for (let i = 0; i < arr.length; i++) {
+                const num = arr[i];
+
+                if (num < cur) {
+                        break;
+                }
+
+                if (num === cur) {
+                        continue;
+                }
+
+                cur++;
+        }
+
+        return cur;
+};
+
+export { maximumElementAfterDecrementingAndRearranging };
