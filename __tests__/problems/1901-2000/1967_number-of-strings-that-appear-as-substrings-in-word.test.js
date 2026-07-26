a8e8b156c0cae3f6fc1a27a7be7a97b2bc0856e5
Anurag Das <ragonscreen@gmail.com>
2026-07-26 09:26:05 +0530

    feat: solve 1967 - Number of Strings That Appear as Substrings in Word - Easy

---
+++ __tests__/problems/1901-2000/1967_number-of-strings-that-appear-as-substrings-in-word.test.js
@@ -0,0 +1,20 @@
+import { describe, expect, test } from 'bun:test';
+import { numOfStrings } from '../../../src/problems/1901-2000/1967_number-of-strings-that-appear-as-substrings-in-word.js';
+
+const testcases = [
+        { patterns: ['a', 'abc', 'bc', 'd'], word: 'abc', expected: 3 },
+        { patterns: ['a', 'b', 'c'], word: 'aaaaabbbbb', expected: 2 },
+        { patterns: ['a', 'a', 'a'], word: 'ab', expected: 3 },
+];
+
+describe('numOfStrings', () => {
+        test.each(
+                structuredClone(testcases),
+        )('numOfStrings($patterns, $word) -> $expected', ({
+                patterns,
+                word,
+                expected,
+        }) => {
+                expect(numOfStrings(patterns, word)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/1901-2000/1967_number-of-strings-that-appear-as-substrings-in-word.js
@@ -0,0 +1,35 @@
+/**
+ * 1967. Number of Strings That Appear as Substrings in Word
+ *
+ * Link: https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
+ * Category: Algorithms
+ * Difficulty: Easy
+ * Date: 2026-07-26
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Array (topic_5)
+ * - String (topic_10)
+ * - Mid Level (position_mid-level)
+ * - Weekly Contest 254 (contest_weekly-contest-254)
+ *
+ * Stats:
+ * - Total Accepted: 247,360
+ * - Total Submissions: 287,131
+ * - Acceptance Rate: 86.1%
+ */
+
+/**
+ * Approach: Simulation
+ * Time Complexity: O(n * k)
+ * Space Complexity: O(1)
+ * `n` = `word.length`, `k` = `sum(patterns[i].length)`
+ *
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+const numOfStrings = (patterns, word) =>
+        patterns.filter((e) => word.includes(e)).length;
+
+export { numOfStrings };
