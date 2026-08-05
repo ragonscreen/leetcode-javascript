608692d7e23f505b1080acb564fe6d4f107cb41d
Anurag Das <ragonscreen@gmail.com>
2026-08-05 23:26:21 +0530

    feat: solve 3754 - Concatenate Non-Zero Digits and Multiply by Sum I - Easy

---
+++ __tests__/problems/3701-3800/3754_concatenate-non-zero-digits-and-multiply-by-sum-i.test.js
@@ -0,0 +1,15 @@
+import { describe, expect, test } from 'bun:test';
+import { sumAndMultiply } from '../../../src/problems/3701-3800/3754_concatenate-non-zero-digits-and-multiply-by-sum-i.js';
+
+const testcases = [
+        { n: 10_203_004, expected: 12_340 },
+        { n: 1000, expected: 1 },
+];
+
+describe('sumAndMultiply', () => {
+        test.each(
+                structuredClone(testcases),
+        )('sumAndMultiply($n) -> $expected', ({ n, expected }) => {
+                expect(sumAndMultiply(n)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/3701-3800/3754_concatenate-non-zero-digits-and-multiply-by-sum-i.js
@@ -0,0 +1,50 @@
+/**
+ * 3754. Concatenate Non-Zero Digits and Multiply by Sum I
+ *
+ * Link: https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/
+ * Category: Algorithms
+ * Difficulty: Easy
+ * Date: 2026-07-08
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Math (topic_8)
+ * - Mid Level (position_mid-level)
+ * - Weekly Contest 477 (contest_weekly-contest-477)
+ *
+ * Stats:
+ * - Total Accepted: 193,538
+ * - Total Submissions: 292,523
+ * - Acceptance Rate: 66.2%
+ */
+
+/**
+ * Approach: Math
+ * Time Complexity: O(log N)
+ * Space Complexity: O(1)
+ * `N` = `n`
+ *
+ * @param {number} n
+ * @return {number}
+ */
+const sumAndMultiply = (n) => {
+        let x = 0;
+        let sum = 0;
+        let f = 1;
+
+        while (n) {
+                const d = n % 10;
+
+                if (d) {
+                        x += d * f;
+                        sum += d;
+                        f *= 10;
+                }
+
+                n = 0 | (n / 10);
+        }
+
+        return x * sum;
+};
+
+export { sumAndMultiply };
