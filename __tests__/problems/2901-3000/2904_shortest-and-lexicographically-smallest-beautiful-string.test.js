de553fba9647ffef07d8e3a06ba7b4a1e5f93d45
Anurag Das <ragonscreen@gmail.com>
2026-08-26 19:37:06 +0530

    feat: solve 2904 - Shortest and Lexicographically Smallest Beautiful String - Medium

---
+++ __tests__/problems/2901-3000/2904_shortest-and-lexicographically-smallest-beautiful-string.test.js
@@ -0,0 +1,27 @@
+import { describe, expect, test } from 'bun:test';
+import { shortestBeautifulSubstring } from '../../../src/problems/2901-3000/2904_shortest-and-lexicographically-smallest-beautiful-string.js';
+
+const testcases = [
+        { s: '100011001', k: 3, expected: '11001' },
+        { s: '1011', k: 2, expected: '11' },
+        { s: '000', k: 1, expected: '' },
+        { s: '111111', k: 6, expected: '111111' },
+        { s: '111111', k: 5, expected: '11111' },
+        { s: '01011101000111110', k: 5, expected: '11111' },
+        { s: '001', k: 1, expected: '1' },
+        { s: '1100100101011001001', k: 7, expected: '1100100101011' },
+];
+
+describe('shortestBeautifulSubstring', () => {
+        test.each(
+                structuredClone(testcases),
+        )('shortestBeautifulSubstring($s, $k) -> $expected', ({
+                s,
+                k,
+                expected,
+        }) => {
+                expect(shortestBeautifulSubstring(s, k)).toStrictEqual(
+                        expected,
+                );
+        });
+});
---
+++ src/problems/2901-3000/2904_shortest-and-lexicographically-smallest-beautiful-string.js
@@ -0,0 +1,64 @@
+/**
+ * 2904. Shortest and Lexicographically Smallest Beautiful String
+ *
+ * Link: https://leetcode.com/problems/shortest-and-lexicographically-smallest-beautiful-string/
+ * Category: Algorithms
+ * Difficulty: Medium
+ * Date: 2026-08-26
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - String (topic_10)
+ * - Sliding Window (topic_55821)
+ * - Senior (position_senior)
+ * - Weekly Contest 367 (contest_weekly-contest-367)
+ *
+ * Stats:
+ * - Total Accepted: 92,374
+ * - Total Submissions: 168,987
+ * - Acceptance Rate: 54.7%
+ */
+
+/**
+ * Approach: Sliding Window
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ * `n` = `s.length`
+ *
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+const shortestBeautifulSubstring = (s, k) => {
+        const n = s.length;
+        let res = '2';
+
+        for (let l = 0, r = 0, cnt = 0, mnLen = n + 1; r < n; r++) {
+                cnt += +s[r];
+
+                if (cnt < k) {
+                        continue;
+                }
+
+                while (cnt === k) {
+                        cnt -= +s[l++];
+                }
+
+                const sz = r - l + 2;
+
+                if (sz > mnLen) {
+                        continue;
+                }
+
+                const sub = s.slice(l - 1, r + 1);
+
+                if (sz < mnLen || (sz === mnLen && sub < res)) {
+                        res = sub;
+                        mnLen = sz;
+                }
+        }
+
+        return res === '2' ? '' : res;
+};
+
+export { shortestBeautifulSubstring };
