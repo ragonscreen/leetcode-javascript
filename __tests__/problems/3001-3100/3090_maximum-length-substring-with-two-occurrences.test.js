2445707b13012ea57b93fc5bf8f11daa8e85b4cf
Anurag Das <ragonscreen@gmail.com>
2026-08-27 02:19:39 +0530

    feat: solve 3090 - Maximum Length Substring With Two Occurrences - Easy

---
+++ __tests__/problems/3001-3100/3090_maximum-length-substring-with-two-occurrences.test.js
@@ -0,0 +1,15 @@
+import { describe, expect, test } from 'bun:test';
+import { maximumLengthSubstring } from '../../../src/problems/3001-3100/3090_maximum-length-substring-with-two-occurrences.js';
+
+const testcases = [
+        { s: 'bcbbbcba', expected: 4 },
+        { s: 'aaaa', expected: 2 },
+];
+
+describe('maximumLengthSubstring', () => {
+        test.each(
+                structuredClone(testcases),
+        )('maximumLengthSubstring($s) -> $expected', ({ s, expected }) => {
+                expect(maximumLengthSubstring(s)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/3001-3100/3090_maximum-length-substring-with-two-occurrences.js
@@ -0,0 +1,51 @@
+/**
+ * 3090. Maximum Length Substring With Two Occurrences
+ *
+ * Link: https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/
+ * Category: Algorithms
+ * Difficulty: Easy
+ * Date: 2026-08-27
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Hash Table (topic_6)
+ * - String (topic_10)
+ * - Sliding Window (topic_55821)
+ * - Mid Level (position_mid-level)
+ * - Weekly Contest 390 (contest_weekly-contest-390)
+ *
+ * Stats:
+ * - Total Accepted: 199,822
+ * - Total Submissions: 257,036
+ * - Acceptance Rate: 77.7%
+ */
+
+/**
+ * Approach: Sliding Window
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * `n` = `s.length`
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const maximumLengthSubstring = (s) => {
+        const map = new Uint32Array(26);
+        let res = -1;
+
+        for (let l = 0, r = 0; r < s.length; r++) {
+                const cr = s.charCodeAt(r) - 97;
+                map[cr]++;
+
+                while (map[cr] > 2) {
+                        const cl = s.charCodeAt(l++) - 97;
+                        map[cl]--;
+                }
+
+                res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+};
+
+export { maximumLengthSubstring };
