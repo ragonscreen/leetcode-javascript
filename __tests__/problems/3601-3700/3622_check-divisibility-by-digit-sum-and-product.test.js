abcbdba4674173df9e201732c46964b48ab8a218
Anurag Das <ragonscreen@gmail.com>
2026-08-31 20:45:29 +0530

    feat: solve 3622 - Check Divisibility by Digit Sum and Product - Easy

---
+++ __tests__/problems/3601-3700/3622_check-divisibility-by-digit-sum-and-product.test.js
@@ -0,0 +1,15 @@
+import { describe, expect, test } from 'bun:test';
+import { checkDivisibility } from '../../../src/problems/3601-3700/3622_check-divisibility-by-digit-sum-and-product.js';
+
+const testcases = [
+        { n: 99, expected: true },
+        { n: 23, expected: false },
+];
+
+describe('checkDivisibility', () => {
+        test.each(
+                structuredClone(testcases),
+        )('checkDivisibility($n) -> $expected', ({ n, expected }) => {
+                expect(checkDivisibility(n)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/3601-3700/3622_check-divisibility-by-digit-sum-and-product.js
@@ -0,0 +1,45 @@
+/**
+ * 3622. Check Divisibility by Digit Sum and Product
+ *
+ * Link: https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/
+ * Category: Algorithms
+ * Difficulty: Easy
+ * Date: 2026-08-31
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Math (topic_8)
+ * - Mid Level (position_mid-level)
+ * - Weekly Contest 459 (contest_weekly-contest-459)
+ *
+ * Stats:
+ * - Total Accepted: 222,559
+ * - Total Submissions: 298,411
+ * - Acceptance Rate: 74.6%
+ */
+
+/**
+ * Approach: Math
+ * Time Complexity: O(log N)
+ * Space Complexity: O(1)
+ * `N` = `n`
+ *
+ * @param {number} n
+ * @return {boolean}
+ */
+const checkDivisibility = (n) => {
+        let x = n;
+        let sum = 0;
+        let mul = 1;
+
+        while (x) {
+                const d = x % 10;
+                sum += d;
+                mul *= d;
+                x = 0 | (x / 10);
+        }
+
+        return n % (sum + mul) === 0;
+};
+
+export { checkDivisibility };
