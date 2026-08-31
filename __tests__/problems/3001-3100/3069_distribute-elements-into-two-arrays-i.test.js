a5b70c6d12e3a3307d382619e4c9cbaf8f5ba7e2
Anurag Das <ragonscreen@gmail.com>
2026-08-31 19:17:32 +0530

    feat: solve 3069 - Distribute Elements Into Two Arrays I - Easy

---
+++ __tests__/problems/3001-3100/3069_distribute-elements-into-two-arrays-i.test.js
@@ -0,0 +1,15 @@
+import { describe, expect, test } from 'bun:test';
+import { resultArray } from '../../../src/problems/3001-3100/3069_distribute-elements-into-two-arrays-i.js';
+
+const testcases = [
+        { nums: [2, 1, 3], expected: [2, 3, 1] },
+        { nums: [5, 4, 3, 8], expected: [5, 3, 4, 8] },
+];
+
+describe('resultArray', () => {
+        test.each(
+                structuredClone(testcases),
+        )('resultArray($nums) -> $expected', ({ nums, expected }) => {
+                expect(resultArray(nums)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/3001-3100/3069_distribute-elements-into-two-arrays-i.js
@@ -0,0 +1,52 @@
+/**
+ * 3069. Distribute Elements Into Two Arrays I
+ *
+ * Link: https://leetcode.com/problems/distribute-elements-into-two-arrays-i/
+ * Category: Algorithms
+ * Difficulty: Easy
+ * Date: 2026-08-31
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Array (topic_5)
+ * - Simulation (topic_61055)
+ * - Mid Level (position_mid-level)
+ * - Weekly Contest 387 (contest_weekly-contest-387)
+ *
+ * Stats:
+ * - Total Accepted: 204,030
+ * - Total Submissions: 243,879
+ * - Acceptance Rate: 83.7%
+ *
+ * Similar Problems:
+ * - divide-array-into-equal-pairs (Easy)
+ * - split-array-largest-sum (Hard)
+ */
+
+/**
+ * Approach: Simulation
+ * Time Complexity: O(n)
+ * Space Complexity: O(1) auxiliary, O(n) total
+ * `n` = `nums.length`
+ *
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const resultArray = (nums) => {
+        const arr1 = [nums[0]];
+        const arr2 = [nums[1]];
+
+        for (let i = 2; i < nums.length; i++) {
+                const num = nums[i];
+
+                if (arr1.at(-1) > arr2.at(-1)) {
+                        arr1.push(num);
+                } else {
+                        arr2.push(num);
+                }
+        }
+
+        return arr1.concat(arr2);
+};
+
+export { resultArray };
