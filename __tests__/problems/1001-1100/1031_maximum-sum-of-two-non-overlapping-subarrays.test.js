90fde05ba3042f5f9f4c0d3645279e67fa17bd64
Anurag Das <ragonscreen@gmail.com>
2026-09-02 18:38:29 +0530

    feat: solve 1031 - Maximum Sum of Two Non-Overlapping Subarrays - Medium

---
+++ __tests__/problems/1001-1100/1031_maximum-sum-of-two-non-overlapping-subarrays.test.js
@@ -0,0 +1,56 @@
+import { describe, expect, test } from 'bun:test';
+import {
+        maxSumTwoNoOverlap,
+        maxSumTwoNoOverlap1,
+} from '../../../src/problems/1001-1100/1031_maximum-sum-of-two-non-overlapping-subarrays.js';
+
+const testcases = [
+        {
+                nums: [0, 6, 5, 2, 2, 5, 1, 9, 4],
+                firstLen: 1,
+                secondLen: 2,
+                expected: 20,
+        },
+        {
+                nums: [3, 8, 1, 3, 2, 1, 8, 9, 0],
+                firstLen: 3,
+                secondLen: 2,
+                expected: 29,
+        },
+        {
+                nums: [2, 1, 5, 6, 0, 9, 5, 0, 3, 8],
+                firstLen: 4,
+                secondLen: 3,
+                expected: 31,
+        },
+];
+
+describe('maxSumTwoNoOverlap', () => {
+        test.each(
+                structuredClone(testcases),
+        )('maxSumTwoNoOverlap($nums, $firstLen, $secondLen) -> $expected', ({
+                nums,
+                firstLen,
+                secondLen,
+                expected,
+        }) => {
+                expect(
+                        maxSumTwoNoOverlap(nums, firstLen, secondLen),
+                ).toStrictEqual(expected);
+        });
+});
+
+describe('maxSumTwoNoOverlap1', () => {
+        test.each(
+                structuredClone(testcases),
+        )('maxSumTwoNoOverlap1($nums, $firstLen, $secondLen) -> $expected', ({
+                nums,
+                firstLen,
+                secondLen,
+                expected,
+        }) => {
+                expect(
+                        maxSumTwoNoOverlap1(nums, firstLen, secondLen),
+                ).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/1001-1100/1031_maximum-sum-of-two-non-overlapping-subarrays.js
@@ -0,0 +1,102 @@
+/**
+ * 1031. Maximum Sum of Two Non-Overlapping Subarrays
+ *
+ * Link: https://leetcode.com/problems/maximum-sum-of-two-non-overlapping-subarrays/
+ * Category: Algorithms
+ * Difficulty: Medium
+ * Date: 2026-09-02
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Array (topic_5)
+ * - Dynamic Programming (topic_13)
+ * - Sliding Window (topic_55821)
+ * - Senior Staff (position_senior-staff)
+ * - Weekly Contest 133 (contest_weekly-contest-133)
+ *
+ * Stats:
+ * - Total Accepted: 89,979
+ * - Total Submissions: 146,446
+ * - Acceptance Rate: 61.4%
+ */
+
+/**
+ * Approach: Prefix Sum + Sliding Window + Dynamic Programming
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ * `n` = `nums.length`
+ *
+ * @param {number[]} nums
+ * @param {number} firstLen
+ * @param {number} secondLen
+ * @return {number}
+ */
+const maxSumTwoNoOverlap = (nums, firstLen, secondLen) => {
+        const n = nums.length;
+        const p = new Uint32Array(n + 1);
+
+        for (let i = 0; i < n; i++) {
+                p[i + 1] = p[i] + nums[i];
+        }
+
+        const sw = (len1, len2) => {
+                let mxTot = -1;
+                let mxLeft = -1;
+
+                for (
+                        let l = 0, r = len1 - 1, i = len1, j = len1 + len2 - 1;
+                        j < n;
+                        l++, r++, i++, j++
+                ) {
+                        const lsum = p[r + 1] - p[l];
+                        mxLeft = Math.max(mxLeft, lsum);
+
+                        const rsum = p[j + 1] - p[i];
+                        mxTot = Math.max(mxTot, mxLeft + rsum);
+                }
+
+                return mxTot;
+        };
+
+        return Math.max(sw(firstLen, secondLen), sw(secondLen, firstLen));
+};
+
+/**
+ * Approach: Prefix Sum
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(n)
+ * `n` = `nums.length`
+ *
+ * @param {number[]} nums
+ * @param {number} firstLen
+ * @param {number} secondLen
+ * @return {number}
+ */
+const maxSumTwoNoOverlap1 = (nums, firstLen, secondLen) => {
+        const n = nums.length;
+        const p = new Uint32Array(n + 1);
+
+        for (let i = 0; i < n; i++) {
+                p[i + 1] = p[i] + nums[i];
+        }
+
+        let res = 0;
+
+        for (let l = 0, r = firstLen - 1; r < n; l++, r++) {
+                const sum1 = p[r + 1] - p[l];
+
+                for (let i = 0, j = secondLen - 1; j < l; i++, j++) {
+                        const sum2 = p[j + 1] - p[i];
+                        res = Math.max(res, sum1 + sum2);
+                }
+
+                for (let i = r + 1, j = i + secondLen - 1; j < n; i++, j++) {
+                        const sum2 = p[j + 1] - p[i];
+                        res = Math.max(res, sum1 + sum2);
+                }
+        }
+
+        return res;
+};
+
+export { maxSumTwoNoOverlap, maxSumTwoNoOverlap1 };
