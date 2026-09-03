b892f26cf2906d2ac34f844b36d50d83aa4685bc
Anurag Das <ragonscreen@gmail.com>
2026-09-03 19:24:44 +0530

    feat: solve 3675 - Minimum Operations to Transform String - Medium

---
+++ __tests__/problems/3601-3700/3675_minimum-operations-to-transform-string.test.js
@@ -0,0 +1,15 @@
+import { describe, expect, test } from 'bun:test';
+import { minOperations } from '../../../src/problems/3601-3700/3675_minimum-operations-to-transform-string.js';
+
+const testcases = [
+        { s: 'yz', expected: 2 },
+        { s: 'a', expected: 0 },
+];
+
+describe('minOperations', () => {
+        test.each(
+                structuredClone(testcases),
+        )('minOperations($s) -> $expected', ({ s, expected }) => {
+                expect(minOperations(s)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/3601-3700/3675_minimum-operations-to-transform-string.js
@@ -0,0 +1,47 @@
+/**
+ * 3675. Minimum Operations to Transform String
+ *
+ * Link: https://leetcode.com/problems/minimum-operations-to-transform-string/
+ * Category: Algorithms
+ * Difficulty: Medium
+ * Date: 2026-09-03
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - String (topic_10)
+ * - Greedy (topic_17)
+ * - Senior (position_senior)
+ * - Weekly Contest 466 (contest_weekly-contest-466)
+ *
+ * Stats:
+ * - Total Accepted: 52,614
+ * - Total Submissions: 84,675
+ * - Acceptance Rate: 62.1%
+ */
+
+/**
+ * Approach: Greedy
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * `n` = `s.length`
+ *
+ * @param {string} s
+ * @return {number}
+ */
+const minOperations = (s) => {
+        const set = new Uint8Array(26);
+
+        for (let i = 0; i < s.length; i++) {
+                set[s.charCodeAt(i) - 97] = 1;
+        }
+
+        for (let i = 1; i < 26; i++) {
+                if (set[i]) {
+                        return 26 - i;
+                }
+        }
+
+        return 0;
+};
+
+export { minOperations };
