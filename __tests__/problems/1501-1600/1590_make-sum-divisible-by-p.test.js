c8166aec77ca18da58d05e2485ced89cef0c7fb5
Anurag Das <ragonscreen@gmail.com>
2026-09-03 19:02:40 +0530

    feat: solve 1590 - Make Sum Divisible by P - Medium

---
+++ __tests__/problems/1501-1600/1590_make-sum-divisible-by-p.test.js
@@ -0,0 +1,25 @@
+import { describe, expect, test } from 'bun:test';
+import { minSubarray } from '../../../src/problems/1501-1600/1590_make-sum-divisible-by-p.js';
+
+const testcases = [
+        { nums: [3, 1, 4, 2], p: 6, expected: 1 },
+        { nums: [6, 3, 5, 2], p: 9, expected: 2 },
+        { nums: [1, 2, 3], p: 3, expected: 0 },
+        {
+                nums: [
+                        8, 32, 31, 18, 34, 20, 21, 13, 1, 27, 23, 22, 11, 15,
+                        30, 4, 2,
+                ],
+                p: 148,
+                expected: 7,
+        },
+        { nums: [3, 1, 3], p: 3, expected: 1 },
+];
+
+describe('minSubarray', () => {
+        test.each(
+                structuredClone(testcases),
+        )('minSubarray($nums, $p) -> $expected', ({ nums, p, expected }) => {
+                expect(minSubarray(nums, p)).toStrictEqual(expected);
+        });
+});
---
+++ src/problems/1501-1600/1590_make-sum-divisible-by-p.js
@@ -0,0 +1,67 @@
+/**
+ * 1590. Make Sum Divisible by P
+ *
+ * Link: https://leetcode.com/problems/make-sum-divisible-by-p/
+ * Category: Algorithms
+ * Difficulty: Medium
+ * Date: 2026-09-03
+ * Author: ragonscreen (https://github.com/ragonscreen/)
+ *
+ * Topics:
+ * - Array (topic_5)
+ * - Hash Table (topic_6)
+ * - Prefix Sum (topic_61068)
+ * - Staff (position_staff)
+ * - Biweekly Contest 35 (contest_biweekly-contest-35)
+ *
+ * Stats:
+ * - Total Accepted: 237,051
+ * - Total Submissions: 556,453
+ * - Acceptance Rate: 42.6%
+ *
+ * Similar Problems:
+ * - find-the-divisibility-array-of-a-string (Medium)
+ * - subarray-sums-divisible-by-k (Medium)
+ */
+
+/**
+ * Approach: Prefix Sum
+ * Time Complexity: O(n)
+ * Space Complexity: O(P)
+ * `n` = `nums.length`, `P` = `p`
+ *
+ * At each step of iteration store the current remainder after modulo division
+ * with `p`. If a pair is found in the map, we discard the previous subarray.
+ * The current subarray is saved with the entry [remainder, index].
+ *
+ * @param {number[]} nums
+ * @param {number} p
+ * @return {number}
+ */
+const minSubarray = (nums, p) => {
+        const n = nums.length;
+        const tgt = nums.reduce((t, c) => t + c, 0) % p;
+
+        if (tgt === 0) {
+                return 0;
+        }
+
+        const map = new Map([[0, -1]]);
+        let res = n;
+
+        for (let i = 0, cur = 0; i < n; i++) {
+                cur = (cur + nums[i]) % p;
+                const pair = (cur - tgt + p) % p;
+                const j = map.get(pair);
+
+                if (j !== undefined) {
+                        res = Math.min(res, i - j);
+                }
+
+                map.set(cur, i);
+        }
+
+        return res === n ? -1 : res;
+};
+
+export { minSubarray };
