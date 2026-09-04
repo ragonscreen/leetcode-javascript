b54d3553b211aa800f62e05ba43b235d5261ce61
Anurag Das <ragonscreen@gmail.com>
2026-09-04 15:16:50 +0530

    fix: revise 3904 - Smallest Stable Index II - Medium

--- src/problems/3901-4000/3904_smallest-stable-index-ii.js
+++ src/problems/3901-4000/3904_smallest-stable-index-ii.js
@@ -4,25 +4,28 @@
  * Link: https://leetcode.com/problems/smallest-stable-index-ii/
  * Category: Algorithms
  * Difficulty: Medium
- * Date: 2026-05-07
+ * Date: 2026-05-07 (Updated: 2026-09-04)
  * Author: ragonscreen (https://github.com/ragonscreen/)
  *
  * Topics:
  *
  * - Array (topic_5)
  * - Prefix Sum (topic_61068)
+ * - Senior (position_senior)
+ * - Weekly Contest 498 (contest_weekly-contest-498)
  *
  * Stats:
  *
- * - Total Accepted: 36,432
- * - Total Submissions: 49,427
- * - Acceptance Rate: 73.7%
+ * - Total Accepted: 43,708
+ * - Total Submissions: 59,746
+ * - Acceptance Rate: 73.2%
  */
 
 /**
  * Approach: Prefix Sum
  * Time Complexity: O(n)
  * Space Complexity: O(n)
+ * `n` = `nums.length`
  *
  * @param {number[]} nums
  * @param {number} k
@@ -30,22 +33,16 @@
  */
 const firstStableIndex = (nums, k) => {
         const n = nums.length;
-        const mins = new Uint32Array(n);
-        let min = nums[n - 1];
+        const mns = new Uint32Array(n);
 
-        for (let i = n - 1; i > -1; i--) {
-                min = Math.min(min, nums[i]);
-                mins[i] = min;
+        for (let i = n - 1, mn = nums[n - 1]; i > -1; i--) {
+                mn = Math.min(mn, nums[i]);
+                mns[i] = mn;
         }
 
-        let max = nums[0];
-
-        for (let i = 0; i < n; i++) {
-                max = Math.max(max, nums[i]);
-
-                if (max - mins[i] <= k) {
-                        return i;
-                }
+        for (let i = 0, mx = -1; i < n; i++) {
+                mx = Math.max(mx, nums[i]);
+                if (mx - mns[i] <= k) return i;
         }
 
         return -1;
