3788211f1cdff3b98df35225639fa6668f7d42a5
Anurag Das <ragonscreen@gmail.com>
2026-09-09 00:48:26 +0530

    fix: revise 0874 - Walking Robot Simulation - Medium

--- __tests__/problems/0801-0900/0874_walking-robot-simulation.test.js
+++ __tests__/problems/0801-0900/0874_walking-robot-simulation.test.js
@@ -6,6 +6,22 @@ const testcases = [
         { commands: [4, -1, 3], obstacles: [], expected: 25 },
         { commands: [4, -1, 4, -2, 4], obstacles: [[2, 4]], expected: 65 },
         { commands: [6, -1, -1, 6], obstacles: [[0, 0]], expected: 36 },
+        {
+                commands: [-1, 8, 6, -1, 1],
+                obstacles: [
+                        [4, -5],
+                        [-3, 3],
+                        [-3, -1],
+                        [-5, -3],
+                        [-3, -5],
+                        [5, -2],
+                        [1, 1],
+                        [-5, -3],
+                        [3, -5],
+                        [5, 0],
+                ],
+                expected: 17,
+        },
 ];
 
 describe('robotSim', () => {
--- src/problems/0801-0900/0874_walking-robot-simulation.js
+++ src/problems/0801-0900/0874_walking-robot-simulation.js
@@ -4,7 +4,7 @@
  * Link: https://leetcode.com/problems/walking-robot-simulation/
  * Category: Algorithms
  * Difficulty: Medium
- * Date: 2026-04-15
+ * Date: 2026-04-15 (Updated: 2026-09-09)
  * Author: ragonscreen (https://github.com/ragonscreen/)
  *
  * Topics:
@@ -25,60 +25,66 @@
  */
 
 /**
- * Approach:
- * Time Complexity: O()
- * Space Complexity: O()
+ * Approach: Simulation
+ * Time Complexity: O(k * n)
+ * Space Complexity: O(m)
+ * `n` = `commands.length`, `m` = `obstacles.length`
+ * `k` = `max(commands[i])`
  *
  * @param {number[]} commands
  * @param {number[][]} obstacles
  * @returns {number}
  */
 const robotSim = (commands, obstacles) => {
-        const OFFSET = 3 * 10 ** 4; // based on constraint -3 * 10^4 <= x[i], y[i] <= 3 * 10^4
-        const WIDTH = 2 * OFFSET + 1;
-        const set = new Set();
+        const ppos = (y, x) => (y << 16) | x;
+        const off = 3 * 1e4;
+        const oset = new Set();
+        const oxset = new Uint8Array(2 * off + 1);
+        const oyset = new Uint8Array(2 * off + 1);
 
         for (const [x, y] of obstacles) {
-                set.add((y + OFFSET) * WIDTH + (x + OFFSET));
+                const yy = -y + off; // invert y to treat north as -Y
+                const xx = x + off;
+                const pos = ppos(yy, xx);
+                oset.add(pos);
+                oyset[yy] = 1;
+                oxset[xx] = 1;
         }
 
-        const dirs = [
-                [1, 0],
-                [0, 1],
-                [-1, 0],
-                [0, -1],
-        ];
+        const d = [-1, 0, 1, 0, -1];
+        let di = 0;
+        let y = off;
+        let x = off;
+        let res = -1;
 
-        let res = 0;
-        let dir = 0;
-        let y = 0;
-        let x = 0;
+        for (const k of commands) {
+                if (k === -2) {
+                        di = (di + 3) % 4;
+                } else if (k === -1) {
+                        di = (di + 1) % 4;
+                } else {
+                        const dy = d[di];
+                        const dx = d[di + 1];
 
-        for (const c of commands) {
-                if (c === -1) {
-                        dir = (dir + 1) % 4;
-                        continue;
-                }
+                        if (oyset[y + dy] || oxset[x + dx]) {
+                                for (let i = 0; i < k; i++) {
+                                        y += dy;
+                                        x += dx;
+                                        const npos = ppos(y, x);
 
-                if (c === -2) {
-                        dir = (dir + 3) % 4;
-                        continue;
-                }
-
-                for (let i = 0; i < c; i++) {
-                        const [dy, dx] = dirs[dir];
-                        const ny = y + dy;
-                        const nx = x + dx;
-
-                        if (set.has((ny + OFFSET) * WIDTH + (nx + OFFSET))) {
-                                break;
+                                        if (oset.has(npos)) {
+                                                y -= dy;
+                                                x -= dx;
+                                                break;
+                                        }
+                                }
+                        } else {
+                                y += k * dy;
+                                x += k * dx;
                         }
 
-                        y = ny;
-                        x = nx;
+                        res = Math.max(res, (y - off) ** 2 + (x - off) ** 2);
                 }
-
-                res = Math.max(res, x ** 2 + y ** 2);
         }
 
         return res;
